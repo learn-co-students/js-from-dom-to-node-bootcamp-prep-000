e258cbe53d2a6f22006f5cd6078231f70a3fb4ef
JOHNNY ARAUJO <araujjohnny@gmail.com>
2017-07-23 19:31:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// write your code below!
+var dog = {
+  [Symbol.iterator](){
+    var pup = 2, kitty = 3;
+    return {
+      [Symbol.iterator]() { return this},
+
+      next() {
+        var animal = pup + kitty;
+        pup *= pup;
+        kitty *= kitty;
+        return {value: animal, done: false};
+      },
+
+      return (v) {
+        console.log("no more aminals");
+        return {value: v, done: true};
+      }
+    };
+  }
+};
+
+for (let v of dog) {
+  console.log(v);
+  if (v > 200){
+    break;
+  };
+};
