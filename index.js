dea91d965306f2b1bb79658d5f1e233fa13533d8
yesI <1317154549@qq.com>
2017-09-17 02:12:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 // write your code below!
 console.log('hello, world')
+
+if(5>2) {
+  console.log('5 is bigger than 2.')
+}
+else {
+  console.log('impossible');
+}
+
+var dog='satisfied'
+if (dog === 'hungry') {
+  console.log('food!!!')
+}else {
+  console.log('full!!!')
+}
