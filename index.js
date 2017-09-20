7f195793ee5c84777bcbf317fbcf5bfc6fe704ed
Kari Lindberg <karisoolindberg@gmail.com>
2017-09-20 03:46:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello, World")
+nodejs index.js
+var name = "Susan"
+if (name === "Susan")  {
+  console.log("The name is correct")
+} else {
+  console.log("Expected the name to be Susan")
+}
