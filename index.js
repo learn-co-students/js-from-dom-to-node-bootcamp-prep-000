58f2f87f4a1493c6f8efcd369669e09e7eb11989
Charlie Sparks <csparks@antioch.edu>
2017-10-17 19:52:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // write your code below!
+var name = "Joe"
+var height = "74"
+var message = `${name} is ${height} inches tall`
+
+module.exports = {name, height, message}
+console.log(message)
+console.log("Hello world");
