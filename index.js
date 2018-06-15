1732f5796bdc840e22e9537067b946602e04491f
Emmanuel D Mendez <manche65@gmail.com>
2018-06-15 10:21:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-// write your code below!
-console.log("Hello, World!")
+// write /ur code below!
+// console.log("Hello, World!")
+
+var name = "Joe"
+var height = 74
+
+if((name = "Susan") === "Susan"){
+
+}else {
+  console.log("check your spelling")
+}
+if((height = 70) === 70){
+
+}else{
+  console.log("enter height correctly")
+}
+//dont worry about this for now
+module.exports = {name, height}
---
+++ other_file.js
@@ -0,0 +1,3 @@
+var index = require("./index.js")
+
+console.log(index.name)
