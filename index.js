8e2a627f62e69fda93ae8b52fff8297fb5b4b767
Alexandria DeCaul <alexandria.decaul@gmail.com>
2019-02-07 20:41:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // write your code below!
+node index.js node
+code.log ("Hello World!")
+node index.js
+console.log ("Hello World!")
+console.log ("Hello World!")
+console.log('Hello World!');
+console.log('Hello World');
+console.log('Hello World');
+
+
