0f9caa26b69c627765e9d49807e6f5a1b6c059f3
Denzel Archie <archie4denzel@gmail.com>
2019-02-15 14:11:28 +0000

    Done.

--- more_js/example_one.js
+++ more_js/example_one.js
@@ -1,5 +1,5 @@
-let person1 = "Peter"
-let person2 = "Withnail"
+let person1 = "Luke"
+let person2 = "Vader"
 
 console.log( 'Hi, how are you, ' + person2 + '?' )
 console.log( 'Oh, I am doing just fine, ' + person1 + ', thanks!' )
