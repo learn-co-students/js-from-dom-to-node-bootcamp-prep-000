8b6339fe842ca581f74f74896e159a1f13d99189
Xinran Che <gracexinran@gmail.com>
2019-04-09 01:04:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // write your code below!
+console.log("Hello, World!")
+let person1 = "Peter"
+let person2 = "Withnail"
+
+console.log( 'Hi, how are you, ' + person2 + '?' )
+console.log( 'Oh, I am doing just fine, ' + person1 + ', thanks!' )
+
+for ( let n = 0; n < 10; n++ ) {
+  console.log( n );
+}
