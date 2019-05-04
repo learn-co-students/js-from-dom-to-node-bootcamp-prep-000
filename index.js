7d0a5b61d1e2b0657f9245510b728c6ec420b908
exuberant-glitch-6816 <natalia.girolamo@gmail.com>
2019-05-04 16:58:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // write your code below!
 console.log("Hello, World!")
+
+let person1 = "Natalia"
+let person2 = "Bia"
+
+console.log( 'Hi, how are you, ' + person2 + '?' )
+console.log( 'Oh, I am doing just fine, ' + person1 + ', thanks!' )
