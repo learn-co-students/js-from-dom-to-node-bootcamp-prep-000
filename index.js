7780d3acb77fae1d25fb4daa4e7c1ed79525f526
Liam Argent <liargent@hotmail.com>
2019-10-28 15:53:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // write your code below!
 console.log("Hello World?");
+
+var words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
+
+for (var i = 0; i < 10; i++) {
+  console.log("Number " + words[i]);
+}
